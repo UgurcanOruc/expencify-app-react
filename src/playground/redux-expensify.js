d923867656c5c9a0d3877ccab09ded4fd53b9cc3
UgurcanOruc <ugurcan-oruc@hotmail.com>
2022-12-01 20:36:48 +0300

    combinedRedusers are used in playground file

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,53 @@
+import { createStore, combineReducers } from 'redux';
+
+// EXPENSES REDUSER
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// FILTERS REDUSER
+const filtersReducerDefaultState = {
+    text: '',
+    sortby: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+
+// STORE CREATION
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+console.log(store.getState());
+
+const demoState = {
+    expenses: [{
+        id: 'dasgfsfdsa',
+        description: 'January rent',
+        note: 'This was the final payment for that address',
+        amount: 54500,
+        createdAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', // dateor amount
+        startDate: undefined,
+        endDate: undefined
+    }
+};
+
