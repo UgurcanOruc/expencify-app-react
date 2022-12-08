aefcea43a5019729dd5f74682f262b3f2649874d
UgurcanOruc <ugurcan-oruc@hotmail.com>
2022-12-08 21:38:51 +0300

    redux connext is loaded and used in some commponents

---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpenseListItem = ({ description, amount, createdAt }) => {
+   return (
+     <div>
+       <p>
+         {description} : {amount}, {createdAt}
+       </p>
+     </div>
+   );
+};
+ 
+export default ExpenseListItem;
--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,7 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Info = () => (
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
     <div>
+      {props.isAdmin && <p>This is private info. Please dont share.</p>}
+      <WrappedComponent {...props} />
     </div>
-)
+  );
+};
+
+const requiredAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? (
+                <WrappedComponent {...props} />
+            ) : (
+                <p>Please login to view info.</p>
+            )}
+        </div>
+    );
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requiredAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={false} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(
+  <AuthInfo isAuthenticated={true} info="There are the details" />,
+  document.getElementById("app")
+);
