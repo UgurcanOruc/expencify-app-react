94698ed5129283061bd2b08ab88d10b7277b6b99
UgurcanOruc <ugurcan@abisteknoloji.com.tr>
2022-12-09 16:01:57 +0300

    form added for AddExpense component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => {
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => {
    return (
      <div>
        <p>
          {description} : {amount}, {createdAt}
+         <button onClick={(e) => {
+          dispatch(removeExpense({id}));
+         }}>Remove</button>
        </p>
      </div>
    );
 };
- 
-export default ExpenseListItem;
+
+export default connect()(ExpenseListItem);
