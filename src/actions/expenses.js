cc35e5f0bd0ff6a55188e1100aa652c00fd1b206
UgurcanOruc <ugurcan-oruc@hotmail.com>
2022-12-22 23:33:59 +0300

    redux thunk added adn configuration made. Also addExpense action is reformed for adding data to Firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,20 +1,36 @@
 import uuid from 'uuid';
+import { Db } from '../firebase/firebase';
+import { collection, addDoc, Timestamp } from "firebase/firestore";
 
-export const addExpense = ({
-  description = "",
-  note = "",
-  amount = 0,
-  createdAt = 0,
-} = {}) => ({
+export const addExpense = (expense) => ({
   type: "ADD_EXPENSE",
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt,
-  },
+  expense
 });
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = "",
+      note = "",
+      amount = 0,
+      createdAt = 0,
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    const expensesRef = collection(Db, 'expenses');
+    addDoc(expensesRef, expense)
+      .then(docRef => {
+        dispatch(
+          addExpense({
+            id: docRef.id,
+            ...expense,
+          })
+        );
+      })
+      .catch((error) => {
+        console.log("Error while adding expense.", error);
+      });
+  }
+}
 
 export const removeExpense = ({ id }) => ({
   type: "REMOVE_EXPENSE",
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, addDoc, collection } from "firebase/firestore"; 
+import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyBa56WBdfo-g4OumSvl9tXXt2igu9kByGw",
@@ -10,16 +10,6 @@ const firebaseConfig = {
   appId: "1:1029998953201:web:46abbdcfb81b611a671f50",
   measurementId: "G-C4Z9KD7MCM",
 };
-
 const firebaseApp = initializeApp(firebaseConfig);
 
-export const db = getFirestore(firebaseApp);
-
-const expensesCollectionRef = collection(db, 'expenses')
-// addDoc(expensesCollectionRef, { name: "Ugurcan Oruc" })
-//   .then(() => {
-//     console.log('Data is saved')
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+export const Db = getFirestore(firebaseApp);
