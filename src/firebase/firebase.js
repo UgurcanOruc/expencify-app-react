b4fdb72fe44882088f0d43d2a7479fed2efc8bf0
UgurcanOruc <ugurcan@abisteknoloji.com.tr>
2023-01-03 21:43:04 +0300

    firebase configurations are changed, test server is removed therefore .env and .development files are deleted.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBa56WBdfo-g4OumSvl9tXXt2igu9kByGw",
+  authDomain: "expensify-app-78188.firebaseapp.com",
+  projectId: "expensify-app-78188",
+  storageBucket: "expensify-app-78188.appspot.com",
+  messagingSenderId: "1029998953201",
+  appId: "1:1029998953201:web:46abbdcfb81b611a671f50",
+  measurementId: "G-C4Z9KD7MCM"
 };
 const firebaseApp = initializeApp(firebaseConfig);
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,6 @@ const path = require("path");
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-if (process.env.NODE_ENV === "test") {
-  require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.decelopment" });
-}
-
 module.exports = (env) => {
   return {
     entry: "./src/app.js",
@@ -48,17 +40,8 @@ module.exports = (env) => {
     },
     plugins: [
       new MiniCssExtractPlugin(),
-      new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
-      }),
     ],
-    devtool: env.production ? "source-map" : "inline-source-map",
+    devtool: "source-map",
     devServer: {
       static: {
         directory: path.join(__dirname, "public"),
